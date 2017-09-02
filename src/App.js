d2e54bbcbfe5c22d91492498a1bfedd16746f300
Onyekachi Mbaike <onyekachi.mbaike@gmail.com>
2017-09-03 00:16:53 +0100

    Add fonts

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import { Route, Switch } from "react-router";
+
+const AppStyle = {
+  height: "100%",
+  width: "100%",
+  background: "#ddd"
+};
+
+class App extends Component {
+  render() {
+    return <div style={AppStyle}>Testing</div>;
+  }
+}
+
+export default App;
